dd1b5fafb1927f00aee69f9bc44df413c961e543
ASAP_A1 <aaronrickymeek@gmail.com>
2024-04-15 15:24:27 +0100

    initializations: add app base configurations

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      maxWidth: {
+        100: "100px",
+        "9xl": "1500px",
+        "screen-3xl": "1650px",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1rem",
+        md: "2rem",
+        lg: "34px",
+        xl: "70px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  plugins: [],
+};
