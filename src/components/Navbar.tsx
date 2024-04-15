bc7316d3c836daaf92851f139ce76a325d16a546
ASAP_A1 <aaronrickymeek@gmail.com>
2024-04-15 15:31:52 +0100

    chore: update packages

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <span>logo</span>
+      <span>home</span>
+      <span>search</span>
+    </div>
+  );
+};
